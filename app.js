430383f661ffa33285813910ca7046e14d2c26b9
Richard0325 <passboy0325@gmail.com>
2022-09-11 23:35:53 +0800

    refactor: add config to manage db connection

--- app.js
+++ app.js
@@ -19,18 +19,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 
-const mongoose = require('mongoose'); // 載入 mongoose
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, { useUnifiedTopology: true }) // 設定連線到 mongoDB
+require('./config/mongoose')
 
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 const routes = require('./routes')
 
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,16 +1,7 @@
-const mongoose = require('mongoose') // 載入 mongoose
+const db = require('../../config/mongoose') // 載入 mongoose
 const Restaurant = require('../restaurant')
 const recentRestaurantList = require('../../restaurant.json').results
-const restaurant = require('../restaurant')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, { useUnifiedTopology: true }) // 設定連線到 mongoD
-
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
   // console.log(Restaurant)
